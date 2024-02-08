0d60fc70ecc6f7691394ee44a48d06baf9ff8714
arihant-jain0 <jainari1208@gmail.com>
2024-02-08 17:56:47 +0530

    update middleware

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -8,17 +8,17 @@ const f = createUploadthing();
 const getUser = async () => await currentUser();
 
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
+
   media: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    // Set permissions and file types for this FileRoute
+
     .middleware(async (req) => {
-      // This code runs on your server before upload
+
       const user = await getUser();
 
-      // If you throw, the user will not be able to upload
+
       if (!user) throw new Error("Unauthorized");
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+   
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,11 @@
-// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
-// Copy the middleware code as it is from the above resource
+
 
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // An array of public routes that don't require authentication.
-// Example (add /api/uploadthing to publicRoutes)
- publicRoutes : ["/api/webhook/clerk", "/api/uploadthing"],
-
-
-  // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes : ["/api/webhook/clerk", "/api/uploadthing"]
+  publicRoutes: ["/", "/api/webhook/clerk", "/api/uploadthing"],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/uploadthing"]
 });
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
